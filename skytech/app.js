3269cd8d71918ba81e3b6d5d496e553a2b796591
kamleshcodes <kamleshkumawat2872@gmail.com>
2025-11-20 11:25:26 +0530

    Add files via upload

---
+++ skytech/app.js
@@ -0,0 +1,12 @@
+let slideIndex = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlides() {
+  slides.forEach(slide => slide.classList.remove("active"));
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+  slides[slideIndex - 1].classList.add("active");
+  setTimeout(showSlides, 3000);
+}
+
+document.addEventListener("DOMContentLoaded", showSlides);
